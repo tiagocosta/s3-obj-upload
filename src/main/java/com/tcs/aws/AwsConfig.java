251536e467c0c3dd18ba97169428202a1ac60b4d
tiagocosta <tiago.costa.silva@gmail.com>
2025-02-07 14:35:30 -0300

    Create AWS Config

---
+++ src/main/java/com/tcs/aws/AwsConfig.java
@@ -0,0 +1,31 @@
+package com.tcs.aws;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "aws.key")
+public class AwsConfig {
+
+    private String access;
+
+    private String secret;
+
+    @Bean
+    public AmazonS3 s3() {
+        var credentials = new BasicAWSCredentials(access, secret);
+        return AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(Regions.US_EAST_1)
+                .build();
+    }
+}
