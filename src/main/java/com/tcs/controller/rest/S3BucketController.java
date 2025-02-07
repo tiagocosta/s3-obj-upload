3daffa4d79db3ed8fbc0ece3d8ca3414c5aa565b
tiagocosta <tiago.costa.silva@gmail.com>
2025-02-07 20:34:54 -0300

    Add create bucket endpoint

--- src/main/java/com/tcs/controller/rest/S3BucketController.java
+++ src/main/java/com/tcs/controller/rest/S3BucketController.java
@@ -5,11 +5,10 @@ import com.tcs.service.BucketService;
 import com.tcs.service.ObjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -31,5 +30,15 @@ public class S3BucketController {
     public ResponseEntity<BucketDto> findByName(@PathVariable String bucketName) {
         var bucket = bucketService.findBucketByName(bucketName);
         return ResponseEntity.ok(new BucketDto(bucket));
+    }
+
+    @PostMapping
+    public ResponseEntity<BucketDto> create(@RequestBody BucketDto bucketDto) {
+        var bucket = bucketService.create(bucketDto.name());
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{bucketName}")
+                .buildAndExpand(bucket.getName())
+                .toUri();
+        return ResponseEntity.created(location).body(new BucketDto(bucket));
     }
 }
